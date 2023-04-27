085d393a91065d5274dcab4ac575c2c13a1068a6
Balu Rama Chandra <baluramachandra90@gmail.com>
2023-04-27 14:28:26 +0700

    final program

--- main.py
+++ main.py
@@ -3,21 +3,23 @@ from dotenv import load_dotenv
 import openai
 
 load_dotenv()
-
 openai.api_key = os.getenv("OPEN_API_KEY")
 
-question = input("\033[31mWhat is your question?\n\033[0m")
+while True:
+  question = input("\033[34mSebutkan Pertanyaan Anda?\n\033[0m")
 
-completion = openai.ChatCompletion.create(
-  model="gpt-3.5-turbo",
-  messages=[
-    {"role": "system", "content": "You are an assistant. Answer the given question."},
-    {"role":"user", "content":question},
-  ]
-)
+  if question.lower() == "selesai":
+    print("\033[31mSampai Jumpa Lagi!\n\033[0m")
+    break
 
-print(question)
+  completion = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+      {"role": "system", "content": "You are an assistant. Answer the given question."},
+      {"role":"user", "content":question},
+    ]
+  )
 
-# print(completion.choices[0].message)
+  print("\033[32m" + completion.choices[0].message.content + "\n")
 
 
